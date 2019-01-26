1bbf84a2719766b86dcbdb1683c17b1dd4ac7d9b
片岡利紗 <risakataoka6@gmail.com>
2019-01-26 16:22:01 +0900

    firstcommit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+      // 以下を追記
+      if (\App::environment('production')) {
+      \URL::forceScheme('https');
+      }
+}
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
